2b75688ab24e9f48bbfaab2d8362b85e404889a1
freearhey <free.arhey@gmail.com>
2019-03-20 01:26:21 +0300

    Update test
    Increased timeout and added delay to each request

--- test/index.js
+++ test/index.js
@@ -5,7 +5,8 @@ const axios = require('axios')
 const path = require('path')
 
 const errorLog = 'error.log'
-const timeout = 10000
+const timeout = 60000
+const delay = 200
 
 let tests = 0
 let channels = 0
@@ -37,6 +38,10 @@ async function testAllLinksIsWorking() {
     const playlist = _loadPlaylist(country.file)
 
     for(let channel of playlist) {
+
+      await new Promise(resolve => {
+        setTimeout(resolve, delay)
+      })
 
       channels++
 
