53c765fce30a104c9ca745e113b5030d8b9bc7e0
freearhey <free.arhey@gmail.com>
2019-04-30 15:45:02 +0300

    Small repo refactoring

--- helpers/generate.js
+++ helpers/generate.js
@@ -2,7 +2,7 @@ const parsers = require('playlist-parser')
 const M3U = parsers.M3U
 const fs = require("fs")
 const path = require('path')
-const helper = require('./helper')
+const urlParser = require('url')
 
 let outputPath = path.resolve(__dirname) + '/../index.all.m3u'
 let channels = 0
@@ -59,13 +59,20 @@ function writeToFile(title, file) {
 }
 
 function addToCache(url) {
-  let id = helper.getUrlPath(url)
+  let id = getUrlPath(url)
 
   cache[id] = true
 }
 
 function checkCache(url) {
-  let id = helper.getUrlPath(url)
+  let id = getUrlPath(url)
 
   return cache.hasOwnProperty(id)
+}
+
+function getUrlPath(u) {
+  let parsed = urlParser.parse(u)
+  let searchQuery = parsed.search || ''
+
+  return parsed.host + parsed.pathname + searchQuery
 }
