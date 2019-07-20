348e7144d6f4f004141c035d8e3f5ef0606f4504
freearhey <free.arhey@gmail.com>
2019-07-20 10:03:31 +0300

    Refactoring helper functions

--- helpers/formatter.js
+++ helpers/formatter.js
@@ -1,81 +1,35 @@
-const parsers = require('playlist-parser')
-const M3U = parsers.M3U
-const fs = require("fs")
-const path = require('path')
-const urlParser = require('url')
+const util = require('./util')
 
 let total = 0
-const supportedGroups = [ 
-  'Auto',
-  'Business', 
-  'CCTV', 
-  'Classic',
-  'Comedy',
-  'Documentary',
-  'Education',
-  'Entertainment', 
-  'Family',
-  'Fashion',
-  'Food', 
-  'General', 
-  'Health', 
-  'History', 
-  'Hobby', 
-  'Kids', 
-  'Legislative',
-  'Lifestyle',
-  'Local', 
-  'Movies', 
-  'Music', 
-  'News', 
-  'Quiz',
-  'Radio', 
-  'Religious',
-  'Sci-Fi', 
-  'Shop', 
-  'Sport', 
-  'Travel', 
-  'Weather', 
-  'XXX'
-]
 
 function init() {
 
-  let countries = loadPlaylist('index.m3u')
-  // countries = countries.slice(0, 4)
+  let countries = util.parsePlaylist('index.m3u')
+  countries = countries.slice(0, 1)
 
   let channels = []
 
   for(let country of countries) {
 
-    const playlist = loadPlaylist(country.file)
+    const playlist = util.parsePlaylist(country.file)
 
     for(let item of playlist) {
 
-      let channel = {
-        title: parseTitle(item),
-        file: item.file,
-        id: parseTvgId(item),
-        name: parseTvgName(item),
-        logo: parseTvgLogo(item),
-        group: parseGroupTitle(item)
-      }
+      let channel = util.parseChannelData(item)
 
       channels.push(channel)
 
     }
 
-    channels = channels.sort(byTitle)
+    channels = util.sortByTitle(channels)
 
-    let outputPath = path.resolve(__dirname) + '/../' + country.file
-
-    fs.writeFileSync(outputPath, '#EXTM3U\n')
+    util.createFile(country.file)
 
     channels.forEach(channel => {
-      writeToFile(outputPath, channel)
-    })
+      const info = `-1 tvg-id="${channel.id}" tvg-name="${channel.name}" tvg-logo="${channel.logo}" group-title="${channel.group}",${channel.title}`
 
-    // console.log(country.title, channels)
+      util.writeToFile(country.file, info, channel.file)
+    })
 
     total += channels.length
 
@@ -86,73 +40,3 @@ function init() {
 init()
 
 console.log(`Total: ${total}.`)
-
-function loadPlaylist(filename) {
-  return M3U.parse(fs.readFileSync(path.resolve(__dirname) + `/../${filename}`, { encoding: "utf8" }))
-}
-
-function writeToFile(outputPath, channel) {
-  let info = `#EXTINF:-1 tvg-id="${channel.id}" tvg-name="${channel.name}" tvg-logo="${channel.logo}" group-title="${channel.group}",${channel.title}`
-
-  fs.appendFileSync(outputPath, info + '\n' + channel.file + '\n')
-}
-
-function getInfo(item) {
-  return (item.artist) ? item.artist + '-' + item.title : item.title
-}
-
-function parseTitle(item) {
-  let info = getInfo(item)
-  let parts = info.split(',')
-
-  return parts[parts.length - 1].trim()
-}
-
-function parseTvgId(item) {
-  let info = getInfo(item)
-  let matches = info.match(/tvg\-id\=\"(.*?)\"/i)
-
-  return (matches && matches.length > 1) ? matches[1] : ''
-}
-
-function parseTvgName(item) {
-  let info = getInfo(item)
-  let matches = info.match(/tvg\-name\=\"(.*?)\"/i)
-
-  return (matches && matches.length > 1) ? matches[1] : ''
-}
-
-function parseTvgLogo(item) {
-  let info = getInfo(item)
-  let matches = info.match(/tvg\-logo\=\"(.*?)\"/i)
-
-  return (matches && matches.length > 1) ? matches[1] : ''
-}
-
-function parseGroupTitle(item) {
-  let info = getInfo(item)
-  let matches = info.match(/group\-title\=\"(.*?)\"/i)
-  let groupTitle = (matches && matches.length > 1) ? matches[1] : ''
-  let groupIndex = supportedGroups.map(g => g.toLowerCase()).indexOf(groupTitle.toLowerCase())
-
-  if(groupIndex === -1) {
-    groupTitle = ''
-  } else {
-    groupTitle = supportedGroups[groupIndex]
-  }
-
-  return groupTitle
-}
-
-function byTitle(a, b) {
-  var nameA = a.title.toLowerCase()
-  var nameB = b.title.toLowerCase()
-  if (nameA < nameB) {
-    return -1
-  }
-  if (nameA > nameB) {
-    return 1
-  }
-
-  return 0
-}
