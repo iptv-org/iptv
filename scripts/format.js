573174c79d732f9222549c80e6042676af70a15d
Aleksandr Statciuk <free.arhey@gmail.com>
2021-08-02 21:12:27 +0300

    Update format.js

--- scripts/format.js
+++ scripts/format.js
@@ -1,5 +1,7 @@
+const categories = require('./helpers/categories')
 const parser = require('./helpers/parser')
 const utils = require('./helpers/utils')
+const file = require('./helpers/file')
 const log = require('./helpers/log')
 
 async function main() {
@@ -11,23 +13,62 @@ async function main() {
     log.print(`\nProcessing '${playlist.url}'...`)
     await parser
       .parsePlaylist(playlist.url)
+      .then(removeWrongCategories)
+      .then(addMissingData)
       .then(sortChannels)
-      .then(p => p.save())
+      .then(playlist => {
+        if (file.read(playlist.url) !== playlist.toString()) {
+          log.print('updated')
+          playlist.updated = true
+        }
+
+        playlist.save()
+      })
   }
 
   log.print('\n')
   log.finish()
 }
 
+async function removeWrongCategories(playlist) {
+  for (const channel of playlist.channels) {
+    if (!channel.category) continue
+    const valid = categories.find(c => c.name === channel.category)
+    if (!valid) {
+      channel.category = ''
+    }
+  }
+
+  return playlist
+}
+
+async function addMissingData(playlist) {
+  for (const channel of playlist.channels) {
+    const code = file.getBasename(playlist.url)
+    // tvg-name
+    if (!channel.tvg.name && code !== 'unsorted' && channel.name) {
+      channel.tvg.name = channel.name.replace(/\"/gi, '')
+    }
+    // tvg-id
+    if (!channel.tvg.id && code !== 'unsorted' && channel.tvg.name) {
+      const id = utils.name2id(channel.tvg.name)
+      channel.tvg.id = id ? `${id}.${code}` : ''
+    }
+    // country
+    if (!channel.countries.length) {
+      const name = utils.code2name(code)
+      channel.countries = name ? [{ code, name }] : []
+      channel.tvg.country = channel.countries.map(c => c.code.toUpperCase()).join(';')
+    }
+  }
+
+  return playlist
+}
+
 async function sortChannels(playlist) {
   const channels = [...playlist.channels]
   utils.sortBy(channels, ['name', 'url'])
-
-  if (JSON.stringify(playlist.channels) !== JSON.stringify(channels)) {
-    log.print('updated')
-    playlist.channels = channels
-    playlist.updated = true
-  }
+  playlist.channels = channels
 
   return playlist
 }
