81ceca15dd069af011ebb51d0fa66ec1a14de1fa
Aleksandr Statciuk <free.arhey@gmail.com>
2021-08-08 13:06:28 +0300

    Update Channel.js

--- scripts/helpers/Channel.js
+++ scripts/helpers/Channel.js
@@ -7,6 +7,7 @@ const nsfwCategories = categories.filter(c => c.nsfw).map(c => c.name)
 
 module.exports = class Channel {
   constructor(data) {
+    this.parsed = data
     this.raw = data.raw
     this.tvg = data.tvg
     this.http = data.http
