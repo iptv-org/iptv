8322631116b67f53129810106a15f6d4fe7b41dd
Aleksandr Statciuk <free.arhey@gmail.com>
2021-09-08 20:46:19 +0300

    Update file.js

--- scripts/helpers/file.js
+++ scripts/helpers/file.js
@@ -1,9 +1,29 @@
 const markdownInclude = require('markdown-include')
 const path = require('path')
+const glob = require('glob')
 const fs = require('fs')
 
 const rootPath = path.resolve(__dirname) + '/../../'
 const file = {}
+
+file.list = function (include = [], exclude = []) {
+  return new Promise(resolve => {
+    glob('channels/*.m3u', function (err, files) {
+      console.log(include, exclude)
+      if (include.length) {
+        include = include.map(filename => `channels/${filename}.m3u`)
+        files = files.filter(filename => include.includes(filename))
+      }
+
+      if (exclude.length) {
+        exclude = exclude.map(filename => `channels/${filename}.m3u`)
+        files = files.filter(filename => !exclude.includes(filename))
+      }
+
+      resolve(files)
+    })
+  })
+}
 
 file.getFilename = function (filename) {
   return path.parse(filename).name
