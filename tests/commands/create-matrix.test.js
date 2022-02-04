cc834a5ec74986a91fce0539fd2387d9e22e032a
Aleksandr Statciuk <github@oiya.xyz>
2022-02-05 02:55:21 +0300

    Update create-matrix.test.js

--- tests/commands/create-matrix.test.js
+++ tests/commands/create-matrix.test.js
@@ -3,7 +3,7 @@ const path = require('path')
 const { execSync } = require('child_process')
 
 beforeEach(() => {
-  fs.copyFileSync('tests/__data__/input/test.db', 'tests/__data__/temp/test.db')
+  fs.copyFileSync('tests/__data__/input/channels.db', 'tests/__data__/temp/channels.db')
 })
 
 afterEach(() => {
@@ -13,7 +13,7 @@ afterEach(() => {
 
 it('can create valid matrix', () => {
   const result = execSync(
-    'DB_FILEPATH=tests/__data__/temp/test.db node scripts/commands/create-matrix.js',
+    'DB_FILEPATH=tests/__data__/temp/channels.db node scripts/commands/create-matrix.js',
     { encoding: 'utf8' }
   )
   expect(result).toBe('::set-output name=matrix::{"cluster_id":[1,3]}\n')
