411171e3f082c17d474c70548e8038c9c2f952de
Aleksandr Statciuk <github@oiya.xyz>
2022-02-05 02:48:48 +0300

    Update create-database.js

--- scripts/store/setters/categories.js
+++
@@ -1,8 +0,0 @@
-const categories = require('../../data/categories')
-
-module.exports = function ({ group_title }) {
-  return group_title
-    .split(';')
-    .map(i => categories[i.toLowerCase()])
-    .filter(i => i)
-}
--- scripts/store/setters/is_nsfw.js
+++
@@ -1,3 +0,0 @@
-module.exports = function ({ categories }) {
-  return Array.isArray(categories) ? categories.filter(c => c.nsfw).length > 0 : false
-}
