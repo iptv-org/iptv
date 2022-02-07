5e1945ce4a2e8d0a40ad20d97e58a900698d9169
Aleksandr Statciuk <github@oiya.xyz>
2022-02-07 03:06:27 +0300

    wip

--- scripts/generators/index_m3u.js
+++ scripts/generators/index_m3u.js
@@ -2,5 +2,6 @@ const api = require('../core/api')
 const _ = require('lodash')
 
 module.exports = async function (streams = []) {
+	streams = _.filter(streams, s => !s.channel || s.channel.is_nsfw === false)
 	return { filepath: 'index.m3u', items: streams }
 }
