002d8804df10af197dc89ba64a654a4e8c3ba8ca
Aleksandr Statciuk <github@oiya.xyz>
2022-02-07 06:11:40 +0300

    Update create-matrix.js

--- scripts/commands/create-matrix.js
+++ scripts/commands/create-matrix.js
@@ -1,7 +1,8 @@
 const { logger, db } = require('../core')
 
 async function main() {
-  const docs = await db.find({}).sort({ cluster_id: 1 })
+  await db.streams.load()
+  const docs = await db.streams.find({}).sort({ cluster_id: 1 })
   const cluster_id = docs.reduce((acc, curr) => {
     if (!acc.includes(curr.cluster_id)) acc.push(curr.cluster_id)
     return acc
