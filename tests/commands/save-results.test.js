f56ea1e44ed9cb95a6646b128abea8af2e0d2f3d
Aleksandr Statciuk <github@oiya.xyz>
2022-02-07 08:41:05 +0300

    Update save-results.test.js

--- tests/commands/save-results.test.js
+++ tests/commands/save-results.test.js
@@ -3,21 +3,21 @@ const path = require('path')
 const { execSync } = require('child_process')
 
 beforeEach(() => {
-  fs.emptyDirSync('tests/__data__/temp')
+  fs.emptyDirSync('tests/__data__/output')
   fs.copyFileSync(
     'tests/__data__/input/database/save-results.streams.db',
-    'tests/__data__/temp/streams.db'
+    'tests/__data__/output/streams.db'
   )
 
   const stdout = execSync(
-    'DB_DIR=tests/__data__/temp LOGS_DIR=tests/__data__/input/logs/load-cluster node scripts/commands/save-results.js',
+    'DB_DIR=tests/__data__/output LOGS_DIR=tests/__data__/input/logs/load-cluster node scripts/commands/save-results.js',
     { encoding: 'utf8' }
   )
 })
 
 it('can save results', () => {
-  expect(content('tests/__data__/temp/streams.db')).toEqual(
-    content('tests/__data__/expected/save-results.streams.db')
+  expect(content('tests/__data__/output/streams.db')).toEqual(
+    content('tests/__data__/expected/database/save-results.streams.db')
   )
 })
 
