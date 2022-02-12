406200e235d0c94516bf11be76a3c047344594f1
Aleksandr Statciuk <github@oiya.xyz>
2022-02-12 04:25:19 +0300

    Update validate.test.js

--- tests/commands/playlist/validate.test.js
+++ tests/commands/playlist/validate.test.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process')
 it('show error if channel name in the blocklist', () => {
   try {
     execSync(
-      'DATA_DIR=tests/__data__/input/data node scripts/commands/playlist/validate.js tests/__data__/input/channels/us_blocked.m3u',
+      'DATA_DIR=tests/__data__/input/data npm run playlist:validate -- tests/__data__/input/channels/us_blocked.m3u',
       {
         encoding: 'utf8'
       }
@@ -11,20 +11,20 @@ it('show error if channel name in the blocklist', () => {
   } catch (err) {
     expect(err.status).toBe(1)
     expect(err.stdout).toBe(
-      `\ntests/__data__/input/channels/us_blocked.m3u\n 2     error    "Fox Sports" is on the blocklist due to claims of copyright holders (https://github.com/github/dmca/blob/master/2020/09/2020-09-16-dfl.md)\n\n1 problems (1 errors, 0 warnings)\n`
+      `\n> playlist:validate\n> node scripts/commands/playlist/validate.js "tests/__data__/input/channels/us_blocked.m3u"\n\n\ntests/__data__/input/channels/us_blocked.m3u\n 2     error    "Fox Sports" is on the blocklist due to claims of copyright holders (https://github.com/github/dmca/blob/master/2020/09/2020-09-16-dfl.md)\n\n1 problems (1 errors, 0 warnings)\n`
     )
   }
 })
 
 it('show warning if channel has wrong id', () => {
   const stdout = execSync(
-    'DATA_DIR=tests/__data__/input/data node scripts/commands/playlist/validate.js tests/__data__/input/channels/wrong_id.m3u',
+    'DATA_DIR=tests/__data__/input/data npm run playlist:validate -- tests/__data__/input/channels/wrong_id.m3u',
     {
       encoding: 'utf8'
     }
   )
 
   expect(stdout).toBe(
-    `\ntests/__data__/input/channels/wrong_id.m3u\n 2     warning  "qib22lAq1L.us" is not in the database\n\n1 problems (0 errors, 1 warnings)\n`
+    `\n> playlist:validate\n> node scripts/commands/playlist/validate.js "tests/__data__/input/channels/wrong_id.m3u"\n\n\ntests/__data__/input/channels/wrong_id.m3u\n 2     warning  "qib22lAq1L.us" is not in the database\n\n1 problems (0 errors, 1 warnings)\n`
   )
 })
