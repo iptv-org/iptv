f151945132c841a9b135306a14fb609d1991ded3
freearhey <7253922+freearhey@users.noreply.github.com>
2023-10-07 07:05:12 +0300

    Update validate.test.ts

--- tests/commands/playlist/validate.test.ts
+++ tests/commands/playlist/validate.test.ts
@@ -1,24 +1,24 @@
 import { execSync } from 'child_process'
 
+type ExecError = {
+  status: number
+  stdout: string
+}
+
 it('show an error if channel name in the blocklist', () => {
   try {
-    const stdout = execSync(
+    execSync(
       'DATA_DIR=tests/__data__/input/data STREAMS_DIR=tests/__data__/input/streams_validate npm run playlist:validate -- us_blocked.m3u',
       {
         encoding: 'utf8'
       }
     )
-    console.log(stdout)
     process.exit(1)
-  } catch (error: unknown) {
-    // @ts-ignore
-    expect(error.status).toBe(1)
-    expect(
-      // @ts-ignore
-      error.stdout.includes(
-        'us_blocked.m3u\n 2     error    "Fox Sports 2 Asia (Thai)" is on the blocklist due to claims of copyright holders (https://github.com/iptv-org/iptv/issues/0000)\n\n1 problems (1 errors, 0 warnings)\n'
-      )
-    ).toBe(true)
+  } catch (error) {
+    expect((error as ExecError).status).toBe(1)
+    expect((error as ExecError).stdout).toContain(
+      'us_blocked.m3u\n 2     error    "Fox Sports 2 Asia (Thai)" is on the blocklist due to claims of copyright holders (https://github.com/iptv-org/iptv/issues/0000)\n\n1 problems (1 errors, 0 warnings)\n'
+    )
   }
 })
 
@@ -30,9 +30,7 @@ it('show a warning if channel has wrong id', () => {
     }
   )
 
-  expect(
-    stdout.includes(
-      'wrong_id.m3u\n 2     warning  "qib22lAq1L.us" is not in the database\n\n1 problems (0 errors, 1 warnings)\n'
-    )
-  ).toBe(true)
+  expect(stdout).toContain(
+    'wrong_id.m3u\n 2     warning  "qib22lAq1L.us" is not in the database\n\n1 problems (0 errors, 1 warnings)\n'
+  )
 })
