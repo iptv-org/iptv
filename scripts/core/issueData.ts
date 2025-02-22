6e26d46defd530832ba4ce5dd3f1e6ee07beab00
freearhey <7253922+freearhey@users.noreply.github.com>
2025-02-22 12:54:00 +0300

    Update scripts

---
+++ scripts/core/issueData.ts
@@ -0,0 +1,32 @@
+import { Dictionary } from '@freearhey/core'
+
+export class IssueData {
+  _data: Dictionary
+  constructor(data: Dictionary) {
+    this._data = data
+  }
+
+  has(key: string): boolean {
+    return this._data.has(key)
+  }
+
+  missing(key: string): boolean {
+    return this._data.missing(key) || this._data.get(key) === undefined
+  }
+
+  getBoolean(key: string): boolean {
+    return Boolean(this._data.get(key))
+  }
+
+  getString(key: string): string {
+    const deleteSymbol = '~'
+
+    return this._data.get(key) === deleteSymbol ? '' : this._data.get(key)
+  }
+
+  getArray(key: string): string[] {
+    const deleteSymbol = '~'
+
+    return this._data.get(key) === deleteSymbol ? [] : this._data.get(key).split(';')
+  }
+}
