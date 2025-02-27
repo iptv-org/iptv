7164934d04c88cd9d99ee04a475cbaeea04dd86f
freearhey <7253922+freearhey@users.noreply.github.com>
2025-02-27 21:19:12 +0300

    Update scripts

--- scripts/models/blocked.ts
+++ scripts/models/blocked.ts
@@ -1,14 +1,17 @@
 type BlockedProps = {
   channel: string
+  reason: string
   ref: string
 }
 
 export class Blocked {
   channel: string
+  reason: string
   ref: string
 
-  constructor({ ref, channel }: BlockedProps) {
+  constructor({ ref, reason, channel }: BlockedProps) {
     this.channel = channel
+    this.reason = reason
     this.ref = ref
   }
 }
