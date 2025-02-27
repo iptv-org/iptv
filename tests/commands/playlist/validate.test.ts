200d8ce66839043546e333030d55030632dde247
freearhey <7253922+freearhey@users.noreply.github.com>
2025-02-27 21:19:01 +0300

    Update validate.test.ts

--- tests/commands/playlist/validate.test.ts
+++ tests/commands/playlist/validate.test.ts
@@ -5,7 +5,7 @@ type ExecError = {
   stdout: string
 }
 
-it('show an error if channel name in the blocklist', () => {
+it('show an error if channel id in the blocklist', () => {
   try {
     execSync(
       'DATA_DIR=tests/__data__/input/data STREAMS_DIR=tests/__data__/input/streams_validate npm run playlist:validate -- us_blocked.m3u',
@@ -16,9 +16,11 @@ it('show an error if channel name in the blocklist', () => {
     process.exit(1)
   } catch (error) {
     expect((error as ExecError).status).toBe(1)
-    expect((error as ExecError).stdout).toContain(
-      'us_blocked.m3u\n 2     error    "Fox Sports 2 Asia (Thai)" is on the blocklist due to claims of copyright holders or NSFW content (https://github.com/iptv-org/iptv/issues/0000)\n\n1 problems (1 errors, 0 warnings)\n'
-    )
+    expect((error as ExecError).stdout).toContain(`us_blocked.m3u
+ 2     error    "FoxSports2Asia.us" is on the blocklist due to claims of copyright holders (https://github.com/iptv-org/iptv/issues/0002)
+ 4     error    "TVN.pl" is on the blocklist due to NSFW content (https://github.com/iptv-org/iptv/issues/0003)
+
+2 problems (2 errors, 0 warnings)`)
   }
 })
 
