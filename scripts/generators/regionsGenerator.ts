ec5cdf0864182e89354d19db1247bb8e6f104789
freearhey <7253922+freearhey@users.noreply.github.com>
2025-04-23 20:56:19 +0300

    Update scripts

--- scripts/generators/regionsGenerator.ts
+++ scripts/generators/regionsGenerator.ts
@@ -1,25 +1,26 @@
-import { Generator } from './generator'
-import { Collection, Storage, Logger } from '@freearhey/core'
+import { Collection, Storage, File } from '@freearhey/core'
 import { Playlist, Region, Stream } from '../models'
 import { PUBLIC_DIR } from '../constants'
+import { Generator } from './generator'
+import { EOL } from 'node:os'
 
 type RegionsGeneratorProps = {
   streams: Collection
   regions: Collection
-  logger: Logger
+  logFile: File
 }
 
 export class RegionsGenerator implements Generator {
   streams: Collection
   regions: Collection
   storage: Storage
-  logger: Logger
+  logFile: File
 
-  constructor({ streams, regions, logger }: RegionsGeneratorProps) {
+  constructor({ streams, regions, logFile }: RegionsGeneratorProps) {
     this.streams = streams
     this.regions = regions
     this.storage = new Storage(PUBLIC_DIR)
-    this.logger = logger
+    this.logFile = logFile
   }
 
   async generate(): Promise<void> {
@@ -35,8 +36,8 @@ export class RegionsGenerator implements Generator {
       const playlist = new Playlist(regionStreams, { public: true })
       const filepath = `regions/${region.code.toLowerCase()}.m3u`
       await this.storage.save(filepath, playlist.toString())
-      this.logger.info(
-        JSON.stringify({ type: 'region', filepath, count: playlist.streams.count() })
+      this.logFile.append(
+        JSON.stringify({ type: 'region', filepath, count: playlist.streams.count() }) + EOL
       )
     })
 
@@ -44,18 +45,20 @@ export class RegionsGenerator implements Generator {
     const internationalPlaylist = new Playlist(internationalStreams, { public: true })
     const internationalFilepath = 'regions/int.m3u'
     await this.storage.save(internationalFilepath, internationalPlaylist.toString())
-    this.logger.info(
+    this.logFile.append(
       JSON.stringify({
         type: 'region',
         filepath: internationalFilepath,
         count: internationalPlaylist.streams.count()
-      })
+      }) + EOL
     )
 
     const undefinedStreams = streams.filter((stream: Stream) => !stream.hasBroadcastArea())
     const playlist = new Playlist(undefinedStreams, { public: true })
     const filepath = 'regions/undefined.m3u'
     await this.storage.save(filepath, playlist.toString())
-    this.logger.info(JSON.stringify({ type: 'region', filepath, count: playlist.streams.count() }))
+    this.logFile.append(
+      JSON.stringify({ type: 'region', filepath, count: playlist.streams.count() }) + EOL
+    )
   }
 }
