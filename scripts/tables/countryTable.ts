66d5c94c2649ad90073f96357114ef59fbb2f870
freearhey <7253922+freearhey@users.noreply.github.com>
2025-07-20 20:28:58 +0300

    Update scripts

--- scripts/tables/countryTable.ts
+++ scripts/tables/countryTable.ts
@@ -1,6 +1,6 @@
 import { Storage, Collection, File } from '@freearhey/core'
 import { HTMLTable, LogParser, LogItem } from '../core'
-import { Country, Subdivision } from '../models'
+import { Country } from '../models'
 import { DATA_DIR, LOGS_DIR, README_DIR } from '../constants'
 import { Table } from './table'
 
@@ -13,11 +13,6 @@ export class CountryTable implements Table {
     const countriesContent = await dataStorage.json('countries.json')
     const countries = new Collection(countriesContent).map(data => new Country(data))
     const countriesGroupedByCode = countries.keyBy((country: Country) => country.code)
-    const subdivisionsContent = await dataStorage.json('subdivisions.json')
-    const subdivisions = new Collection(subdivisionsContent).map(data => new Subdivision(data))
-    const subdivisionsGroupedByCode = subdivisions.keyBy(
-      (subdivision: Subdivision) => subdivision.code
-    )
 
     const parser = new LogParser()
     const logsStorage = new Storage(LOGS_DIR)
@@ -25,27 +20,6 @@ export class CountryTable implements Table {
     const parsed = parser.parse(generatorsLog)
 
     let data = new Collection()
-
-    parsed
-      .filter((logItem: LogItem) => logItem.type === 'subdivision')
-      .forEach((logItem: LogItem) => {
-        const file = new File(logItem.filepath)
-        const code = file.name().toUpperCase()
-        const [countryCode, subdivisionCode] = code.split('-') || ['', '']
-        const country = countriesGroupedByCode.get(countryCode)
-
-        if (country && subdivisionCode) {
-          const subdivision = subdivisionsGroupedByCode.get(code)
-          if (subdivision) {
-            data.add([
-              `${country.name}/${subdivision.name}`,
-              `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${subdivision.name}`,
-              logItem.count,
-              `<code>https://iptv-org.github.io/iptv/${logItem.filepath}</code>`
-            ])
-          }
-        }
-      })
 
     parsed
       .filter((logItem: LogItem) => logItem.type === 'country')
