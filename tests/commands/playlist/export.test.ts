a27ba0c7d0c6e2a0ca97f66c1867327199e5bc45
freearhey <7253922+freearhey@users.noreply.github.com>
2025-12-04 18:32:06 +0300

    Update tests

--- tests/commands/playlist/export.test.ts
+++ tests/commands/playlist/export.test.ts
@@ -3,20 +3,20 @@ import { execSync } from 'child_process'
 import fs from 'fs-extra'
 
 const ENV_VAR =
-  'cross-env DATA_DIR=tests/__data__/input/data STREAMS_DIR=tests/__data__/input/api_generate API_DIR=tests/__data__/output/.api'
+  'cross-env DATA_DIR=tests/__data__/input/data STREAMS_DIR=tests/__data__/input/playlist_export API_DIR=tests/__data__/output/.api'
 
 beforeEach(() => {
   fs.emptyDirSync('tests/__data__/output')
 })
 
-describe('api:generate', () => {
+describe('playlist:export', () => {
   it('can create streams.json', () => {
-    const cmd = `${ENV_VAR} npm run api:generate`
+    const cmd = `${ENV_VAR} npm run playlist:export`
     const stdout = execSync(cmd, { encoding: 'utf8' })
     if (process.env.DEBUG === 'true') console.log(cmd, stdout)
 
     expect(content('tests/__data__/output/.api/streams.json')).toMatchObject(
-      content('tests/__data__/expected/api_generate/.api/streams.json')
+      content('tests/__data__/expected/playlist_export/.api/streams.json')
     )
   })
 })
