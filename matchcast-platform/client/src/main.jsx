3d3364b4985dfa39b6bc13b73e8365c9791617e9
bronzy99 <43916940+bronzy99@users.noreply.github.com>
2026-07-05 21:13:23 +0000

    Add MatchCast main entry

---
+++ matchcast-platform/client/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
